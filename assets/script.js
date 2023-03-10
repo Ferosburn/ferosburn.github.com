95e75e3504d44032942a63305a7de968a573a66c
Su'udi Khoirul Anam <udi.khoirul@gmail.com>
2023-03-10 17:43:20 +0700

    add new content

--- assets/script.js
+++ assets/script.js
@@ -1,225 +1,279 @@
-const projects =
-  [
-    // {
-    //   projectName: 'Portofolio V3',
-    //   link: "https://ferosburn.github.io/",
-    //   repository: "https://github.com/Ferosburn/ferosburn.github.io",
-    //   languages: [
-    //     "HTML", "CSS", "JavaScript"
-    //   ],
-    //   languagePercentage: [
-    //     {
-    //       langName: "HTML",
-    //       percentage: 43.1
-    //     },
-    //     {
-    //       langName: "CSS",
-    //       percentage: 20.2
-    //     },
-    //     {
-    //       langName: "JavaScript",
-    //       percentage: 36.7
-    //     }
-    //   ],
-    //   date: "Juni 2022",
-    //   images: [],
-    //   description: "Portofolio pribadi versi ketiga"
-    // },
-    {
-      projectName: 'To-Do-List',
-      link: "https://ferosburn.github.io/todo-list/",
-      repository: "https://github.com/Ferosburn/todo-list",
-      languages: [
-        "HTML", "CSS", "JavaScript"
-      ],
-      frameworks: [
-        "ReactJS"
-      ],
-      languagePercentage: [
-        {
-          langName: "HTML",
-          percentage: 21.4
-        },
-        {
-          langName: "CSS",
-          percentage: 29.1
-        },
-        {
-          langName: "JavaScript",
-          percentage: 49.5
-        }
-      ],
-      date: "Juni 2022",
-      images: [
-        "./assets/images/Todo List.png"
-      ],
-      description: "Proyek pribadi dibuat untuk memenuhi tugas membuat todo-list menggunakan framework ReactJS dari pelatihan DTS FGA Progate ReactJS for Web Development.",
-    },
-    {
-      projectName: 'Kalkulator',
-      link: "https://ferosburn.github.io/kalkulator/",
-      repository: "https://github.com/Ferosburn/kalkulator",
-      languages: [
-        "HTML", "CSS", "JavaScript"
-      ],
-      languagePercentage: [
-        {
-          langName: "HTML",
-          percentage: 43.1
-        },
-        {
-          langName: "CSS",
-          percentage: 36.7
-        },
-        {
-          langName: "JavaScript",
-          percentage: 20.2
-        }
-      ],
-      date: "Juni 2022",
-      images: [
-        "./assets/images/Kalkulator 1.png",
-        "./assets/images/Kalkulator 2.png"
-      ],
-      description: "Proyek pribadi dibuat untuk memenuhi tugas membuat kalkulator menggunakan JavaScript dari pelatihan DTS FGA Progate ReactJS for Web Development."
-    },
-    {
-      projectName: 'UndangApp',
-      link: "https://undangapp2.herokuapp.com/",
-      repository: "https://github.com/Ferosburn/undangapp",
-      languages: [
-        "HTML", "CSS", "Ruby"
-      ],
-      frameworks: [
-        "Ruby on Rails", "Bootstrap"
-      ],
-      languagePercentage: [
-        {
-          langName: "HTML",
-          percentage: 41.7
-        },
-        {
-          langName: "CSS",
-          percentage: 1.2
-        },
-        {
-          langName: "Ruby",
-          percentage: 52.1
-        },
-        {
-          langName: "Other",
-          percentage: 5
-        },
-      ],
-      date: "April 2022",
-      images: [
-        "./assets/images/UndangApp 1.png",
-        "./assets/images/UndangApp 2.png",
-        "./assets/images/UndangApp 3.png",
-        "./assets/images/UndangApp 4.png",
-        "./assets/images/UndangApp 5.png"
-      ],
-      description: "Proyek pribadi iseng setelah melakukan pelatihan DTS FGA Progate Full Stack Developer with Ruby (HTML, CSS, Ruby, Ruby on Rails). Saya membuat proyek ini untuk mengukur dan mengembangkan kemampuan saya dalam membuat sebuah web app."
-    },
-    {
-      projectName: 'Masakan Nusantara',
-      link: "https://ferosburn.github.io/masakan-nusantara/",
-      repository: "https://github.com/Ferosburn/masakan-nusantara",
-      languages: ["HTML", "CSS"],
-      languagePercentage: [
-        {
-          langName: "HTML",
-          percentage: 53.7
-        },
-        {
-          langName: "CSS",
-          percentage: 46.3
-        }
-      ],
-      date: "Maret 2022",
-      images: [
-        "./assets/images/Masakan Nusantara Desktop.png",
-        "./assets/images/Masakan Nusantara Tablet.png",
-        "./assets/images/Masakan Nusantara Phone.png"
-      ],
-      description: "Proyek ini dikerjakan secara tim dan dibuat untuk tugas HTML dan CSS dari pelatihan DTS FGA Progate Full Stack Developer with Ruby (HTML, CSS, Ruby, Ruby on Rails)."
-    },
-    {
-      projectName: 'Portofolio',
-      link: "https://ferosburn-portofolio.herokuapp.com/",
-      repository: "https://github.com/Ferosburn/ferosburn-portofolio",
-      languages: ["HTML", "CSS"],
-      languagePercentage: [
-        {
-          langName: "HTML",
-          percentage: 55.4
-        },
-        {
-          langName: "CSS",
-          percentage: 44.2
-        },
-        {
-          langName: "Other",
-          percentage: 0.4
-        }
-      ],
-      date: "September 2021",
-      images: [
-        "./assets/images/Portofolio 1.png",
-        "./assets/images/Portofolio 2.png"
-      ],
-      description: "Tugas akhir individu untuk pelatihan dari Kode ID Belajar Langkah Fundamental untuk Menjadi Web Developer"
-    }
-  ];
+// const projects = [
+//   {
+//     projectName: "Random Quote Machine",
+//     link: "https://ferosburn.github.io/random-quote-machine/",
+//     repository: "https://github.com/Ferosburn/random-quote-machine",
+//     languages: ["HTML", "CSS", "JavaScript"],
+//     frameworks: ["ReactJS"],
+//     languagePercentage: [
+//       {
+//         langName: "HTML",
+//         percentage: 37.5,
+//       },
+//       {
+//         langName: "CSS",
+//         percentage: 7.5,
+//       },
+//       {
+//         langName: "JavaScript",
+//         percentage: 55,
+//       },
+//     ],
+//     date: "Maret 2023",
+//     images: [
+//       "./assets/images/Random Quote Machine 1.png",
+//       "./assets/images/Random Quote Machine 2.png",
+//     ],
+//     description:
+//       "Sebuah halaman web yang dapat menghasilkan kutipan secara random dan dapat membagikan ke twitter sebagai sebuah tweet. Submission untuk Front End Development Libraries Projects dari freeCodeCamp",
+//   },
+//   {
+//     projectName: "Bookshelf API",
+//     link: "",
+//     repository: "https://github.com/Ferosburn/bookshelf-api",
+//     languages: ["JavaScript"],
+//     frameworks: ["NodeJS"],
+//     languagePercentage: [
+//       {
+//         langName: "JavaScript",
+//         percentage: 100,
+//       },
+//     ],
+//     date: "Februari 2023",
+//     images: [],
+//     description:
+//       "Submission api untuk kelas Dicoding Belajar Membuat Aplikasi Back-End untuk Pemula",
+//   },
+//   {
+//     projectName: "Skilvul UI",
+//     link: "https://www.figma.com/file/shM2nG7UIjTM3sANszc0fK/Lomba-UI%2FUX-IAA-UKDW-x-Skilvul?node-id=0%3A1&t=6YtAr1Fu26sKwjke-1",
+//     repository: "",
+//     languages: [],
+//     languagePercentage: [],
+//     date: "Juli 2022",
+//     images: [
+//       "./assets/images/Skilvul UI 1.png",
+//       "./assets/images/Skilvul UI 2.png",
+//     ],
+//     description:
+//       "Submission desain UI homepage website Skilvul untuk lomba UI/UX IAA UKDW x Skilvul 2022",
+//   },
+//   {
+//     projectName: "Bookshelf App",
+//     link: "https://ferosburn.github.io/book-shelf/",
+//     repository: "https://github.com/Ferosburn/book-shelf",
+//     languages: ["HTML", "CSS", "JavaScript"],
+//     languagePercentage: [
+//       {
+//         langName: "HTML",
+//         percentage: 20.1,
+//       },
+//       {
+//         langName: "CSS",
+//         percentage: 29.4,
+//       },
+//       {
+//         langName: "JavaScript",
+//         percentage: 50.5,
+//       },
+//     ],
+//     date: "Juli 2022",
+//     images: [
+//       "./assets/images/Bookshelf App 1.png",
+//       "./assets/images/Bookshelf App 2.png",
+//     ],
+//     description:
+//       "Submission webpage untuk kelas Dicoding Belajar Membuat Front-End Web untuk Pemula",
+//   },
+//   {
+//     projectName: "To-Do-List",
+//     link: "https://ferosburn.github.io/todo-list/",
+//     repository: "https://github.com/Ferosburn/todo-list",
+//     languages: ["HTML", "CSS", "JavaScript"],
+//     frameworks: ["ReactJS"],
+//     languagePercentage: [
+//       {
+//         langName: "HTML",
+//         percentage: 21.4,
+//       },
+//       {
+//         langName: "CSS",
+//         percentage: 29.1,
+//       },
+//       {
+//         langName: "JavaScript",
+//         percentage: 49.5,
+//       },
+//     ],
+//     date: "Juni 2022",
+//     images: ["./assets/images/Todo List.png"],
+//     description:
+//       "Proyek pribadi dibuat untuk memenuhi tugas membuat todo-list menggunakan framework ReactJS dari pelatihan DTS FGA Progate ReactJS for Web Development.",
+//   },
+//   {
+//     projectName: "Kalkulator",
+//     link: "https://ferosburn.github.io/kalkulator/",
+//     repository: "https://github.com/Ferosburn/kalkulator",
+//     languages: ["HTML", "CSS", "JavaScript"],
+//     languagePercentage: [
+//       {
+//         langName: "HTML",
+//         percentage: 43.1,
+//       },
+//       {
+//         langName: "CSS",
+//         percentage: 36.7,
+//       },
+//       {
+//         langName: "JavaScript",
+//         percentage: 20.2,
+//       },
+//     ],
+//     date: "Juni 2022",
+//     images: [
+//       "./assets/images/Kalkulator 1.png",
+//       "./assets/images/Kalkulator 2.png",
+//     ],
+//     description:
+//       "Proyek pribadi dibuat untuk memenuhi tugas membuat kalkulator menggunakan JavaScript dari pelatihan DTS FGA Progate ReactJS for Web Development.",
+//   },
+//   {
+//     projectName: "UndangApp",
+//     link: "https://undangapp2.herokuapp.com/",
+//     repository: "https://github.com/Ferosburn/undangapp",
+//     languages: ["HTML", "CSS", "Ruby"],
+//     frameworks: ["Ruby on Rails", "Bootstrap"],
+//     languagePercentage: [
+//       {
+//         langName: "HTML",
+//         percentage: 41.7,
+//       },
+//       {
+//         langName: "CSS",
+//         percentage: 1.2,
+//       },
+//       {
+//         langName: "Ruby",
+//         percentage: 52.1,
+//       },
+//       {
+//         langName: "Other",
+//         percentage: 5,
+//       },
+//     ],
+//     date: "April 2022",
+//     images: [
+//       "./assets/images/UndangApp 1.png",
+//       "./assets/images/UndangApp 2.png",
+//       "./assets/images/UndangApp 3.png",
+//       "./assets/images/UndangApp 4.png",
+//       "./assets/images/UndangApp 5.png",
+//     ],
+//     description:
+//       "Proyek pribadi iseng setelah melakukan pelatihan DTS FGA Progate Full Stack Developer with Ruby (HTML, CSS, Ruby, Ruby on Rails). Saya membuat proyek ini untuk mengukur dan mengembangkan kemampuan saya dalam membuat sebuah web app.",
+//   },
+//   {
+//     projectName: "Masakan Nusantara",
+//     link: "https://ferosburn.github.io/masakan-nusantara/",
+//     repository: "https://github.com/Ferosburn/masakan-nusantara",
+//     languages: ["HTML", "CSS"],
+//     languagePercentage: [
+//       {
+//         langName: "HTML",
+//         percentage: 53.7,
+//       },
+//       {
+//         langName: "CSS",
+//         percentage: 46.3,
+//       },
+//     ],
+//     date: "Maret 2022",
+//     images: [
+//       "./assets/images/Masakan Nusantara Desktop.png",
+//       "./assets/images/Masakan Nusantara Tablet.png",
+//       "./assets/images/Masakan Nusantara Phone.png",
+//     ],
+//     description:
+//       "Proyek ini dikerjakan secara tim dan dibuat untuk tugas HTML dan CSS dari pelatihan DTS FGA Progate Full Stack Developer with Ruby (HTML, CSS, Ruby, Ruby on Rails).",
+//   },
+//   {
+//     projectName: "Portofolio",
+//     link: "https://ferosburn-portofolio.herokuapp.com/",
+//     repository: "https://github.com/Ferosburn/ferosburn-portofolio",
+//     languages: ["HTML", "CSS"],
+//     languagePercentage: [
+//       {
+//         langName: "HTML",
+//         percentage: 55.4,
+//       },
+//       {
+//         langName: "CSS",
+//         percentage: 44.2,
+//       },
+//       {
+//         langName: "Other",
+//         percentage: 0.4,
+//       },
+//     ],
+//     date: "September 2021",
+//     images: [
+//       "./assets/images/Portofolio 1.png",
+//       "./assets/images/Portofolio 2.png",
+//     ],
+//     description:
+//       "Tugas akhir individu untuk pelatihan dari Kode ID Belajar Langkah Fundamental untuk Menjadi Web Developer",
+//   },
+// ];
 
-const content = document.querySelector('.content');
-const navToggleBtn = document.querySelector('.toggle-btn');
-const navLinks = document.querySelector('.nav-links');
-const projectLinks = document.querySelector('.project-links');
 
-window.addEventListener('DOMContentLoaded', () => {
+const content = document.querySelector(".content");
+const navToggleBtn = document.querySelector(".toggle-btn");
+const navLinks = document.querySelector(".nav-links");
+const projectLinks = document.querySelector(".project-links");
+
+window.addEventListener("DOMContentLoaded", () => {
   displayProjectItems(projects);
   displayNavProject(projects);
-  const navProjectItems = document.querySelectorAll('.link-item');
-  navProjectItems.forEach(item => {
-    item.addEventListener('click', () => {
-      navLinks.classList.toggle('hidden');
-    })
-  })
+  const navProjectItems = document.querySelectorAll(".link-item");
+  navProjectItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navLinks.classList.toggle("hidden");
+    });
+  });
 
-  const items = document.querySelectorAll('article');
+  const items = document.querySelectorAll("article");
   items.forEach((item) => {
-    const expand = item.querySelector('.expand');
-    const info = item.querySelector('.info');
-    const close = item.querySelector('.close');
-    const imgBtns = item.querySelectorAll('.img-btn');
-    const projectImg = item.querySelector('.project-image')
+    const expand = item.querySelector(".expand");
+    const info = item.querySelector(".info");
+    const close = item.querySelector(".close");
+    const imgBtns = item.querySelectorAll(".img-btn");
+    const projectImg = item.querySelector(".project-image");
 
     let currentItem = 0;
 
-    expand.addEventListener('click', () => {
+    expand.addEventListener("click", () => {
       toggleInfo();
-    })
+    });
 
-    close.addEventListener('click', () => {
+    close.addEventListener("click", () => {
       toggleInfo();
-    })
+    });
 
     const toggleInfo = () => {
-      info.classList.toggle('hidden');
-      expand.classList.toggle('hidden');
-    }
+      info.classList.toggle("hidden");
+      expand.classList.toggle("hidden");
+    };
 
-    imgBtns.forEach(imgBtn => {
+    imgBtns.forEach((imgBtn) => {
       imgBtn.addEventListener("click", (e) => {
         const index = item.id;
         const images = projects[index].images;
-        if (e.currentTarget.classList.contains('prev')) {
+        if (e.currentTarget.classList.contains("prev")) {
           currentItem--;
           if (currentItem < 0) {
             currentItem = images.length - 1;
           }
-        } else if (e.currentTarget.classList.contains('next')) {
+        } else if (e.currentTarget.classList.contains("next")) {
           currentItem++;
           if (currentItem > images.length - 1) {
             currentItem = 0;
@@ -227,24 +281,24 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         const image = images[currentItem];
         projectImg.src = image;
-      })
-    })
-  })
+      });
+    });
+  });
 });
 
-navToggleBtn.addEventListener('click', (e) => {
-  navLinks.classList.toggle('hidden');
-})
+navToggleBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("hidden");
+});
 
 const displayNavProject = (projectItems) => {
   let displayProject = projectItems.map((item, index) => {
     return `<li>
               <a class='link-item' href='#${index}'><i class="fa-solid fa-chevron-right"></i>${item.projectName}</a>
-            </li>`
-  })
-  displayProject = `<p>Projects</p>${displayProject.join('')}`;
+            </li>`;
+  });
+  displayProject = `<p>Projects</p>${displayProject.join("")}`;
   projectLinks.innerHTML = displayProject;
-}
+};
 
 const displayProjectItems = (projectItems) => {
   let displayProject = projectItems.map((item, index) => {
@@ -256,20 +310,32 @@ const displayProjectItems = (projectItems) => {
               <h2>${item.projectName}</h2>
               <div class='image-container'>
                 <button class='img-btn prev'><i class="fa-solid fa-chevron-left"></i></button>
-                  <img class='project-image' src='${item.images[0]}' alt='no-image-available'>
+                  <img class='project-image' src='${
+                    item.images[0]
+                  }' alt='no-image-available'>
                 <button class='img-btn next'><i class="fa-solid fa-chevron-right"></i></button>
               </div>
               <h3 class="date">${item.date}</h3>
               <p class="link">
-                <a target="_blank" href="${item.link}"><i class="fa-solid fa-link"></i></a>
-                <a target="_blank" href="${item.repository}"><i class="fa-brands fa-github"></i></a>
+                <a class="${
+                  item.link === "" ? "hidden" : ""
+                }" target="_blank" href="${
+      item.link
+    }"><i class="fa-solid fa-link"></i></a>
+                <a class="${
+                  item.repository === "" ? "hidden" : ""
+                }" target="_blank" href="${
+      item.repository
+    }"><i class="fa-brands fa-github"></i></a>
               </p>
               <div class='desc'>
                 <p>${item.description}
                 <span class='expand'>Selengkapnya</span></p>
                 <div class='info hidden'>
                   <div class="close">X</div>
-                  <p><span class='info-item'>Languages</span>: ${item.languages.join(', ')}</p>
+                  <p><span class='info-item'>Languages</span>: ${item.languages.join(
+                    ", "
+                  )}</p>
                   <p><span class='info-item'>Framework/Library</span>: ${frameworks}</p>
                   <div>
                     <p><span class='info-item'>Language Proportion</span>:</p>
@@ -277,26 +343,26 @@ const displayProjectItems = (projectItems) => {
                   </div>
                 </div>
               </div>
-            </article>`
+            </article>`;
   });
-  displayProject = displayProject.join('');
+  displayProject = displayProject.join("");
   content.innerHTML = displayProject;
-}
+};
 
 const handleFrameworks = (item) => {
   let frameworks;
   if (item.frameworks) {
-    frameworks = item.frameworks.join(', ');
+    frameworks = item.frameworks.join(", ");
   } else {
-    frameworks = '-';
+    frameworks = "-";
   }
-  return frameworks
-}
+  return frameworks;
+};
 
 const handleLangPercentage = (item) => {
   let languagePercentage = item.languagePercentage.map((lang) => {
     return `<div class='lang ${lang.langName}' style='width: ${lang.percentage}%;'></div>`;
   });
-  languagePercentage = languagePercentage.join('');
+  languagePercentage = languagePercentage.join("");
   return languagePercentage;
-}
+};
